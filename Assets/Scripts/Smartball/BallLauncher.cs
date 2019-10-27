9c68f1a5930659b0700df705682c4cdf12e31755
watanabe02 <>
2019-10-27 17:45:41 +0900

    Add charging state UI.

--- Assets/Scripts/Smartball/BallLauncher.cs
+++ Assets/Scripts/Smartball/BallLauncher.cs
@@ -8,9 +8,14 @@ public class BallLauncher : MonoBehaviour
     [SerializeField] Transform m_MuzzleT;
 
     [SerializeField] float m_LaunchForce = 5.0f;
+    [SerializeField] float m_MaxChargingRange = 10.0f;
+    [SerializeField] float m_MaxForce = 3.0f;
 
 
     Ball m_ChamberBall;
+    Vector2 m_MouseClickPos = Vector2.zero;
+    float m_ChargingRate;
+    bool m_IsCharging;
 
 
     void OnTriggerEnter(Collider other)
@@ -19,23 +24,51 @@ public class BallLauncher : MonoBehaviour
 
     }
 
+    void Awake()
+    {
+        m_MaxChargingRange = Screen.height * 0.5f;
+        Debug.Log("m_MaxChargingRange:" + m_MaxChargingRange);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            LaunchBall();
+            LaunchBall(m_LaunchForce);
         }
+        CheckMouseState();
     }
 
-    void LaunchBall()
+    void LaunchBall(float force)
     {
         if (m_ChamberBall == null) {
             Debug.Log("ball is null!!");
             return; }
         m_ChamberBall.transform.position = m_MuzzleT.position;
         Rigidbody rb = m_ChamberBall.GetComponent<Rigidbody>();
-        rb.AddForce(m_MuzzleT.forward * m_LaunchForce);
+        rb.AddForce(m_MuzzleT.forward * force);
         m_ChamberBall = null;
+    }
+
+    void CheckMouseState()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_MouseClickPos = Input.mousePosition;
+            m_IsCharging = true;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float diffY = m_MouseClickPos.y - Input.mousePosition.y;
+            diffY = Mathf.Clamp(diffY, 0.0f, m_MaxChargingRange);
+            m_ChargingRate = diffY / m_MaxChargingRange;
+            ChargingStateUI.SetChargingState(m_ChargingRate);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            LaunchBall(m_MaxForce * m_ChargingRate);
+            ChargingStateUI.SetChargingState(0.0f);
+        }
     }
 
 }
---
+++ Assets/Scripts/Smartball/ChargingStateUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargingStateUI : MonoBehaviour
+{
+
+    [SerializeField] List<SpriteRenderer> m_SpriteRenList;
+
+
+    static ChargingStateUI m_Instance;
+
+    
+
+
+    void Awake()
+    {
+        m_Instance = this;
+        SetChargingState(0.0f);
+    }
+
+    public static void SetChargingState(float rate)
+    {
+        int floor = Mathf.Clamp((int)Mathf.Floor(rate * 10.0f), 0, m_Instance.m_SpriteRenList.Count - 1);
+        Debug.Log("floor:" + floor);
+        for (int i = 0; i < floor; i++)
+        {
+            m_Instance.m_SpriteRenList[i].color = Color.white;
+        }
+
+        m_Instance.m_SpriteRenList[floor].color = Color.white * rate;
+
+        int ceil = Mathf.Clamp((int)Mathf.Ceil(rate * 10.0f), 0, m_Instance.m_SpriteRenList.Count - 1);
+        Debug.Log("ceil:" + ceil);
+        for (int i = ceil; i < m_Instance.m_SpriteRenList.Count; i++)
+        {
+            m_Instance.m_SpriteRenList[i].color = Color.clear;
+        }
+    }
+
+}
