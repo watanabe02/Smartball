f6db061f980f0cb64adc99ef368756ac99052154
watanabe02 <>
2019-10-27 13:49:45 +0900

    Add BallLoader.

---
+++ Assets/Scripts/Smartball/BallLoader.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLoader : MonoBehaviour
+{
+    
+    [SerializeField] Transform m_BottomPosT;
+    [SerializeField] Transform m_RaycastT;
+
+
+    static BallLoader m_Instance;
+    Queue<int> m_AddingBallTask = new Queue<int>();
+    const float m_RaycastTimeInterval = 0.1f;
+    const float m_BallInterval = 0.001f;
+    float m_LastRaycastTime;
+    const int m_MaxLoadingBallCount = 5;
+
+
+    public static BallLoader insatnce { get { return m_Instance; } }
+
+
+    void Awake()
+    {
+        m_Instance = this;
+        UnityEditor.EditorApplication.isPaused = true;
+
+    }
+
+    void Start()
+    {
+        AddBall(20);
+    }
+
+    void Update()
+    {
+        if (m_AddingBallTask.Count == 0) { return; }
+        if (IsBallInLoadingSpace()) { return; }
+        InstallBall(m_AddingBallTask.Dequeue());
+    }
+
+    public static void AddBall(int ballCount)
+    {
+        int count = ballCount;
+        while (count > m_MaxLoadingBallCount)
+        {
+            m_Instance.m_AddingBallTask.Enqueue(m_MaxLoadingBallCount);
+            count -= m_MaxLoadingBallCount;
+        }
+        m_Instance.m_AddingBallTask.Enqueue(count);
+    }
+
+    bool IsBallInLoadingSpace()
+    {
+        if (Time.time - m_LastRaycastTime < m_RaycastTimeInterval)
+        {
+            Debug.Log("return time");
+            return true;
+        }
+
+        Ray ray = new Ray(m_RaycastT.position, -m_RaycastT.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1.0f))
+        {
+            if (hit.collider.tag == "Ball")
+            {
+                Debug.Log("return ball");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void InstallBall(int ballCount)
+    {
+        Debug.Log("InstallBall(" + ballCount + ")");
+        float ballDiameter = BallManager.ballDiameter;
+        for (int i = 0; i < ballCount; i++)
+        {
+            Vector3 ballPos = m_BottomPosT.position + (m_BallInterval + ballDiameter) * i * m_BottomPosT.up;
+            BallManager.CreateBall(ballPos);
+        }
+        m_LastRaycastTime = Time.time;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Smartball/BallManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject m_BallPrefab;
+
+    static BallManager m_Instance;
+    List<Ball> m_BallList = new List<Ball>();
+
+    public static BallManager instance { get { return m_Instance; } }
+    public static float ballDiameter { get { return m_Instance.m_BallPrefab.transform.localScale.x; } }
+
+    void Awake()
+    {
+        m_Instance = this;
+    }
+
+    public static void CreateBall(Vector3 worldPos)
+    {
+        GameObject newBallObj = Instantiate<GameObject>(m_Instance.m_BallPrefab, worldPos, Quaternion.identity, m_Instance.transform);
+        m_Instance.m_BallList.Add(newBallObj.GetComponent<Ball>());
+    }
+
+}
